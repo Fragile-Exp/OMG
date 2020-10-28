8e67dc96b61b437f5f9f95468093562e9f2a62b0
ahrdydlf <72778310+ahrdydlf@users.noreply.github.com>
2020-10-28 17:55:50 +0900

    comutting first

--- OMG/src/main/java/com/omg/comutting/dao/CommutingDaoImpl.java
+++ OMG/src/main/java/com/omg/comutting/dao/CommutingDaoImpl.java
@@ -1,5 +1,10 @@
 package com.omg.comutting.dao;
 
-public class CommutingDaoImpl {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class CommutingDaoImpl {
+	
+	/** LOG */
+	final static Logger LOG = LoggerFactory.getLogger(CommutingDaoImpl.class);
 }
