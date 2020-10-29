3ccdae7b575d41ef6ec455090d4c7a2fd5e363f0
ahrdydlf <72778310+ahrdydlf@users.noreply.github.com>
2020-10-29 17:25:53 +0900

    광민dao push

---
+++ OMG/src/main/java/com/omg/comutting/dao/ComuttingDao.java
@@ -0,0 +1,18 @@
+package com.omg.comutting.dao;
+
+
+
+public interface ComuttingDao {
+	
+//	public int InsertAttendTime(com.omg.cmn.DTO dto);
+//
+//	public int doLeaveUpdate(DTO dto);
+//
+//	public ComuttidoSelectOne(DTO dto);
+//
+//	doDelete
+//
+//	doSelectALL
+//
+//	doSelectDeptTime
+}
---
+++ OMG/src/main/java/com/omg/comutting/dao/PresentState.java
@@ -0,0 +1,41 @@
+package com.omg.comutting.dao;
+/**
+ * 현재 출결 
+ *
+ */
+public enum PresentState {
+	
+	자리비움(30,null),퇴근(20,자리비움),근무중(10,퇴근);
+	
+	private final int value;
+	private final PresentState next;
+	
+	PresentState(int value,PresentState next) {
+		this.value = value;
+		this.next = next;
+	}
+	
+	// CODE return
+	public int intValue() {
+		return this.value;
+	}
+	
+	//next CODE_NM return
+	public PresentState nextState() {
+		return this.next;
+	}
+	
+	// CODE -> _NM
+	public static  PresentState valueOf(int value) {
+		
+		switch(value) {
+		case 10 : return 근무중;
+		case 20 : return 퇴근;
+		case 30 : return 자리비움;
+		
+		default:
+			throw new AssertionError("Unknown value :" + value);
+
+		}
+	}
+}
