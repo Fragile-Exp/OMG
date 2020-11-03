b47c10e17d1cefd7e1870f4fa747040862f0a9f5
JaeHoon-Kim95 <wrhgs123@naver.com>
2020-11-03 17:28:03 +0900

    view Template
    뷰 템플릿

---
+++ OMG/src/main/java/com/omg/cmn/viewtest/viewController.java
@@ -0,0 +1,34 @@
+package com.omg.cmn.viewtest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class viewController {
+	final Logger LOG = LoggerFactory.getLogger(viewController.class);
+	
+	@RequestMapping(value="view/main.do",method=RequestMethod.GET)
+	public String main_view() {
+		LOG.debug("== user_view ==");
+		
+		return "index";
+	}
+	
+	@RequestMapping(value="view/blank.do",method=RequestMethod.GET)
+	public String blank_view() {
+		LOG.debug("== user_view ==");
+		
+		return "blank";
+	}
+	
+	@RequestMapping(value="view/org.do",method=RequestMethod.GET)
+	public String org_view() {
+		LOG.debug("== user_view ==");
+		
+		return "org";
+	}
+
+}
