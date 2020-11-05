cdab62d2a5d198f47840cf6257267d0bffc1b3e7
ub-choi <72732559+ub-choi@users.noreply.github.com>
2020-11-05 18:00:02 +0900

    employee controller

--- OMG/src/main/java/com/omg/employee/controller/EmployeeController.java
+++ OMG/src/main/java/com/omg/employee/controller/EmployeeController.java
@@ -1,5 +1,9 @@
 package com.omg.employee.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +14,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.gson.Gson;
 import com.omg.cmn.Message;
+import com.omg.cmn.Search;
+import com.omg.cmn.StringUtil;
 import com.omg.employee.domain.EmployeeVO;
 import com.omg.employee.service.EmployeeServiceImpl;
 
@@ -21,8 +27,139 @@ public class EmployeeController {
 	EmployeeServiceImpl employeeService;
 	
 	public EmployeeController() {}
+	
+	
+	@RequestMapping(value="employee/doSelectList.do",method = RequestMethod.GET
+			,produces = "application/json;charset=UTF-8"
+			)
+	@ResponseBody
+	public String doSelectList(HttpServletRequest req,Search search) {
+		LOG.debug("1==================");
+        LOG.debug("=search="+search);
+        LOG.debug("==================");
+        if(search.getPageNum()==0) {
+        	search.setPageNum(1);
+        }
+        
+        //페이지 사이즈 기본값 처리 
+        if(search.getPageSize()==0) {
+        	search.setPageSize(10);
+        }
+        
+        //검색구분
+        search.setSearchDiv(StringUtil.nvl(search.getSearchDiv(), ""));
+        
+        //검색어
+        search.setSearchWord(StringUtil.nvl(search.getSearchWord(), ""));
+        LOG.debug("2==================");
+        LOG.debug("=null처리 search="+search);
+        LOG.debug("==================");       
+        
+        List<EmployeeVO> list= this.employeeService.doSelectList(search);
+        
+		Gson gson=new Gson();
+        
+		String json = gson.toJson(list);
+        LOG.debug("3==================");
+        LOG.debug("=json="+json);
+        LOG.debug("==================");   		
+		
+		
+		return json;
+	}
+	
+	@RequestMapping(value="employee/doUpdate.do",method = RequestMethod.GET
+			,produces = "application/json;charset=UTF-8"
+			)
+	@ResponseBody
+	public String doUpdate(EmployeeVO employee) {
+		LOG.debug("1==================");
+        LOG.debug("=user="+employee);
+        LOG.debug("==================");	
+        
+        int flag=this.employeeService.doUpdate(employee);
+        LOG.debug("2==================");
+        LOG.debug("=flag="+flag);
+        LOG.debug("==================");     
+        
+        Message message=new Message();
+        message.setMsgId(String.valueOf(flag));
+        
+        if(flag ==1 ) {
+        	message.setMsgContents(employee.getName()+" 님이 수정 되었습니다.");
+        }else {
+        	message.setMsgContents(employee.getName()+" 님 수정 실패.");
+        }
+        
+        Gson   gson=new Gson();
+        String json = gson.toJson(message);
+        LOG.debug("3==================");
+        LOG.debug("=json="+json);
+        LOG.debug("==================");		
+		return json;
+	}
+	
+	
+	@RequestMapping(value="employee/doSelectOne.do",method = RequestMethod.GET
+			,produces = "application/json;charset=UTF-8"
+			)
+	@ResponseBody
+	public String doSelectOne(EmployeeVO employee) {
+		LOG.debug("==================");
+        LOG.debug("=employee="+employee);
+        LOG.debug("==================");
+        
+        EmployeeVO outVO=employeeService.doSelectOne(employee.getEmployee_id());
+        LOG.debug("==================");
+        LOG.debug("=outVO="+outVO);
+        LOG.debug("==================");
+        
+        Gson gson=new Gson();
+        String json = gson.toJson(outVO);
+        LOG.debug("==================");
+        LOG.debug("=json="+json);
+        LOG.debug("==================");
+         
+        return json;
+	}
+	
+	
+	@RequestMapping(value="employee/doDelete.do",method = RequestMethod.GET
+			,produces = "application/json;charset=UTF-8"
+			)
+	@ResponseBody
+	public String doDelete (EmployeeVO employee) {
+		LOG.debug("==================");
+        LOG.debug("=employee="+employee);
+        LOG.debug("==================");
+        
+        int flag=this.employeeService.doDelete(employee);
+        LOG.debug("==================");
+        LOG.debug("=flag="+flag);
+        LOG.debug("=================="); 
+        
+        //메시지 처리
+        Message  message=new Message();
+        message.setMsgId(flag+"");
+        
+        if(flag ==1 ) {
+        	message.setMsgContents(employee.getName()+" 님이 삭제 되었습니다.");
+        }else {
+        	message.setMsgContents(employee.getName()+" 님 삭제 실패.");
+        }
+        
+        Gson gson=new Gson();
+        String json = gson.toJson(message);
+        LOG.debug("==================");
+        LOG.debug("=json="+json);
+        LOG.debug("==================");         
+        
+		return json;
+	}
+	
+	
 
-	@RequestMapping(value="user/doInsert.do",method = RequestMethod.POST
+	@RequestMapping(value="employee/doInsert.do",method = RequestMethod.POST
 			,produces = "application/json;charset=UTF-8"
 			)
 	@ResponseBody
--- OMG/src/test/java/com/omg/employee/TestEmployeeController.java
+++ OMG/src/test/java/com/omg/employee/TestEmployeeController.java
@@ -13,6 +13,7 @@ import java.util.List;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
@@ -33,6 +34,7 @@ import org.springframework.web.context.WebApplicationContext;
 
 import com.google.gson.Gson;
 import com.omg.cmn.Message;
+import com.omg.cmn.Search;
 import com.omg.employee.domain.EmployeeVO;
 import com.omg.employee.service.EmployeeServiceImpl;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -77,6 +79,172 @@ public class TestEmployeeController {
 	public void tearDown() throws Exception {
 	}
 	
+	@Test
+	public void doSelectList() throws Exception {
+		Search search=new Search("","",10,1);
+		
+		MockHttpServletRequestBuilder createMessage = MockMvcRequestBuilders.get("/employee/doSelectList.do")
+				.param("searchDiv", search.getSearchDiv())
+				.param("searchWord", search.getSearchWord())
+				.param("pageSize", search.getPageSize()+"")
+				.param("pageNum", search.getPageNum()+"");
+		
+		ResultActions resultActions = this.mockMvc.perform(createMessage)
+	            .andExpect(MockMvcResultMatchers.content().contentType("application/json;charset=UTF-8"))
+	            .andExpect(status().is2xxSuccessful());
+		
+		String result = resultActions.andDo(print()).andReturn().getResponse().getContentAsString();
+		LOG.debug("=================" + result);
+		LOG.debug("=result=" + result);
+		LOG.debug("=================" + result);
+		
+		
+	}
+	
+	@Test
+	@Ignore
+	public void doUpdate() throws Exception {
+		//데이터 삭제
+		for(EmployeeVO vo:employees) {
+			doDelete(vo);
+		}
+		
+		//단건 입력
+		int flag=doInsert(employees.get(0));
+		assertThat(flag, is(1));
+		
+		//데이터 수정
+		EmployeeVO employee01=employees.get(0);
+		employee01.setEmployee_id(employee01.getEmployee_id());
+		employee01.setPassword(employee01.getPassword()+"_U");
+		employee01.setDept_no(0);
+		employee01.setPosition_no(0);
+		employee01.setCell_phone(0);
+		employee01.setEmail(employee01.getEmail()+"_U");
+		employee01.setAddress(employee01.getAddress()+"_U");
+		employee01.setHoliday(0);
+		employee01.setImg_code(employee01.getImg_code()+"_U");
+		
+		int upFlag=doUpdateController(employee01);
+		assertThat(upFlag,is(1));
+		
+		
+		 
+	}
+	
+	
+	@Test
+	@Ignore
+	public void addAndGet() throws Exception{
+		//데이터 삭제
+		for(EmployeeVO vo:employees) {
+			doDelete(vo);
+		}
+		
+		//데이터 입력
+		for(EmployeeVO vo:employees) { 
+			int flag=doInsert(vo);
+			assertThat(flag, is(1)); 
+		}
+		
+		//기존 데이터와 비교
+		for(EmployeeVO vo: employees) {
+			EmployeeVO vsEmployee=doSelectOne(vo);
+			checkUser(vo, vsEmployee);
+		}
+	}
+	
+	public int doUpdateController(EmployeeVO employee) throws Exception {
+		LOG.debug(">>>>>>>>>>>>>>>>>>>>>>>>");
+	    LOG.debug("=doUpdateController()=");
+		
+	    MockHttpServletRequestBuilder createMessage = MockMvcRequestBuilders.get("/employee/doUpdate.do")
+	    											.param("employee_id", employee.getEmployee_id())
+										 			.param("password", employee.getPassword())
+										 			.param("name", employee.getName())
+										 			.param("dept_no", employee.getDept_no()+"")
+										 			.param("position_no", employee.getPosition_no()+"")
+										 			.param("cell_phone", employee.getCell_phone()+"")
+										 			.param("email", employee.getEmail())
+										 			.param("address", employee.getAddress())
+										 			.param("hire_date", employee.getHire_date())
+										 			.param("birth_day", employee.getBirth_day())
+										 			.param("holiday", employee.getHoliday()+"")
+										 			.param("img_code", employee.getImg_code())
+										 			;
+	    ResultActions resultActions = this.mockMvc.perform(createMessage)
+	            .andExpect(MockMvcResultMatchers.content().contentType("application/json;charset=UTF-8"))
+	            .andExpect(status().is2xxSuccessful()).andExpect(jsonPath("$.msgId", is("1")))// 데이터 꺼내보기
+	      ;
+	    String result = resultActions.andDo(print()).andReturn().getResponse().getContentAsString();
+
+	    LOG.debug("=result=" + result);
+	    // json --> message
+	    Gson gson = new Gson();
+	    Message outVO = gson.fromJson(result, Message.class);
+	    LOG.debug("=message=" + outVO);
+	    LOG.debug(">>>>>>>>>>>>>>>>>>>>>>>>");
+
+	    return Integer.valueOf(outVO.getMsgId());
+	
+	}				
+	
+	private void checkUser(EmployeeVO orgEmployee, EmployeeVO vsEmployee) {
+    	assertThat(orgEmployee.getEmployee_id(),is(vsEmployee.getEmployee_id()));
+    	assertThat(orgEmployee.getPassword(),is(vsEmployee.getPassword()));
+    	assertThat(orgEmployee.getName(), is(vsEmployee.getName()));
+    	assertThat(orgEmployee.getDept_no(), is(vsEmployee.getDept_no()));
+    	assertThat(orgEmployee.getPosition_no(), is(vsEmployee.getPosition_no()));
+    	assertThat(orgEmployee.getCell_phone(), is(vsEmployee.getCell_phone()));
+    	assertThat(orgEmployee.getEmail(),is(vsEmployee.getEmail()));
+    	assertThat(orgEmployee.getAddress(), is(vsEmployee.getAddress()));
+    	assertThat(orgEmployee.getHoliday(), is(vsEmployee.getHoliday()));
+    	assertThat(orgEmployee.getImg_code(), is(vsEmployee.getImg_code()));
+    	
+    }
+	
+	public EmployeeVO doSelectOne(EmployeeVO employee) throws Exception {
+		MockHttpServletRequestBuilder  createMessage =MockMvcRequestBuilders.get("/employee/doSelectOne.do")
+													.param("employee_id", employee.getEmployee_id())
+													;
+		ResultActions resultActions = mockMvc.perform(createMessage)
+				.andExpect(MockMvcResultMatchers.content().contentType("application/json;charset=UTF-8"))
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(jsonPath("$.employee_id", is(employee.getEmployee_id())));
+
+		String result=resultActions.andDo(print())
+						.andReturn()
+						.getResponse().getContentAsString();
+		
+		LOG.debug("==========================");
+		LOG.debug("=result="+result);
+		LOG.debug("==========================");
+		//json to User
+		Gson gson= new Gson();
+		EmployeeVO outVO=gson.fromJson(result, EmployeeVO.class);
+		LOG.debug("==========================");
+		LOG.debug("=outVo="+outVO);
+		LOG.debug("==========================");
+		return outVO;
+	}
+	
+	public void doDelete(EmployeeVO employee) throws Exception {
+		MockHttpServletRequestBuilder  createMessage =MockMvcRequestBuilders.get("/employee/doDelete.do")
+													.param("employee_id", employee.getEmployee_id())
+													;
+		ResultActions resultActions = mockMvc.perform(createMessage)
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json;charset=UTF-8"))
+                .andExpect(status().is2xxSuccessful())
+                ;
+		
+		String result=resultActions.andDo(print())
+				.andReturn()
+				.getResponse().getContentAsString();
+	
+		LOG.debug("==========================");
+		LOG.debug("=result="+result);
+		LOG.debug("==========================");
+	}
 	
 
 	public int doInsert(EmployeeVO employee) throws Exception {
