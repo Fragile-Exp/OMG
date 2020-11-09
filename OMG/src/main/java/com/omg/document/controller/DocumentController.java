501973b4b1db6e335fa453a8b63f8237c18f3999
JungByeongwoon <72732437+JungByeongwoon@users.noreply.github.com>
2020-11-09 19:26:10 +0900

    org dept/position view
    dept/org List, insert 컨트롤러 연결 완료

--- OMG/src/main/java/com/omg/document/controller/DocumentController.java
+++ OMG/src/main/java/com/omg/document/controller/DocumentController.java
@@ -1,20 +1,10 @@
 package com.omg.document.controller;
 
-import javax.annotation.Resource;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.View;
-
-import com.google.gson.Gson;
-import com.omg.cmn.Message;
-import com.omg.document.domain.DocumentVO;
-import com.omg.document.service.DocumentService;
 
 
 
