6fa0d0411448c3157a1095176acd266d5b9f7984
Byeongwoon Jung <cholei2236@naver.com>
2020-11-19 10:46:20 +0900

    last

--- OMG/src/main/java/com/omg/document/controller/DocumentController.java
+++ OMG/src/main/java/com/omg/document/controller/DocumentController.java
@@ -176,7 +176,7 @@ public class DocumentController {
 		inFileVO.setFileCode(SeleteOne.getFileCode());
 		List<AttachmentVO> fileList = attachmentServiceImpl.doSelectList(inFileVO);
 
-		empVO.setEmployee_id(SeleteOne.getOkUser());
+		empVO.setName(SeleteOne.getOkUser());
 		LOG.debug("empVO" + empVO);
 		empVO = documentService.doempIdSelete(empVO);
 		LOG.debug("emp" + empVO);
